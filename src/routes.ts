1439da20c8cf73e9e22f8ce9d83867f4682eed40
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-12-22 21:12:39 -0300

    project settings

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/teste', (req: Request, res: Response) => {
+    return res.json({
+        nome: 'Everton Carvalho',
+        trabalho: 'Desenvolvedor',
+        empresa: "Conecto"
+    })
+});
+
+export { router };
