d6c433ee898b7426102241bb6bda46f86424cdc7
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-12-29 12:26:53 -0300

    encrypting the password

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,6 @@
+import prismaClient from '../../prisma';
+import { hash } from 'bcryptjs';
+
 interface UserRequest {
     name: string;
     email: string;
@@ -6,9 +9,40 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({ name, email, password }: UserRequest) {
-        console.log({ name, email, password });
 
-        return { ok: 'aaaaaaaaa' }
+        //verificar se ele enviou um email
+        if (!email) {
+            throw new Error("Email incorrect");
+        }
+
+        //verificar se esse email já está cadastrado na plataforma
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        });
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        //cadastrar usuário no banco de dados
+        const passwordHash = await hash(password, 8);
+
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: passwordHash
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        return user;
     }
 }
 
