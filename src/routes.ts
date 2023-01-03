eb5f77fb55d5d9d64e6da236535c7165642b543b
Everton Carvalho <everton.carvalho.dc@gmail.com>
2023-01-03 13:45:59 -0300

    Logging in and generating JWT token

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,19 @@ import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const authUserController = new AuthUserController();
+const detailUserController = new DetailUserController();
 
 // -- ROTAS USER --
 router.post('/users', createUserController.handle);
+
 router.post('/session', authUserController.handle);
+
+router.get('/me', detailUserController.handle);
 
 export { router };
--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface AuthRequest {
     email: string;
@@ -26,10 +27,25 @@ class AuthUserService {
             throw new Error("User/password incorrect");
         }
 
-        // Gerar token JWT e devolver os dados do usuário como id, name e email
+        // Se deu tudo certo, vamos gerar um token para o usuário
 
+        const token = sign({
+            name: user.name,
+            email: user.email
+        },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
 
-        return { ok: true }
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
