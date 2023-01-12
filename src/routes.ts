0adc9f981525b6e020817ebcad05529694c57d8a
Everton Carvalho <everton.carvalho.dc@gmail.com>
2023-01-12 12:17:59 -0300

    route products

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/product/CreateProductController';
 
 import { isAutheticated } from './middlewares/isAutheticated';
 
@@ -15,6 +16,8 @@ const authUserController = new AuthUserController();
 const detailUserController = new DetailUserController();
 const createCategoryController = new CreateCategoryController();
 const listCategoryController = new ListCategoryController();
+const createProductController = new CreateProductController();
+
 
 // -- ROTAS USER --
 router.post('/users', createUserController.handle);
@@ -27,5 +30,8 @@ router.get('/me', isAutheticated, detailUserController.handle);
 router.post('/category', isAutheticated, createCategoryController.handle);
 
 router.get('/category', isAutheticated, listCategoryController.handle);
+
+// -- ROTAS PRODUCT
+router.post('/product', isAutheticated, createProductController.handle);
 
 export { router };
