7f2afe7bcbb3f97ec41f81ad90507611b46abf3c
Everton Carvalho <everton.carvalho.dc@gmail.com>
2023-01-24 14:02:36 -0300

    sending order

--- src/routes.ts
+++ src/routes.ts
@@ -12,6 +12,7 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
 
 import { isAutheticated } from './middlewares/isAutheticated';
 
@@ -32,6 +33,7 @@ const createOrderController = new CreateOrderController();
 const removeOrderController = new RemoveOrderController();
 const addItemController = new AddItemController();
 const removeItemController = new RemoveItemController();
+const sendOrderController = new SendOrderController();
 
 // -- ROTAS USER --
 router.post('/users', createUserController.handle);
@@ -58,6 +60,8 @@ router.delete('/order', isAutheticated, removeOrderController.handle);
 router.post('/order/add', isAutheticated, addItemController.handle);
 
 router.delete('/order/remove', isAutheticated, removeItemController.handle);
+
+router.put('/order/send', isAutheticated, sendOrderController.handle);
 
 
 export { router };
